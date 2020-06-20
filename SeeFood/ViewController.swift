de4d937bdc91d35a33b0ce1f81e0f480758e3773
pateldhruvil96 <patel.dhruvil96@gmail.com>
2020-06-20 16:25:06 +0530

    1)Enabled Camera Func

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker=UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate=self
+        imagePicker.sourceType = .camera
+      //  imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing=true
+        
     }
-
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage //this is done to check if it is not nill then proceed further
+       {
+         imageView.image=userPickedImage
+        }
+      
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil) //so when button is clicked it will trigger the imagepicker actions which consists of above mention actions ie opens camera,allows editing n all.
+    }
+    
+    
 }
 
